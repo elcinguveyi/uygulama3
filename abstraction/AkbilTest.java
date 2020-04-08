adcbbd397c4fa2f0a65f15da4d1d5c4557435282
elcinguveyi <34247982+elcinguveyi@users.noreply.github.com>
2020-04-08 19:20:06 +0300

    Update AkbilTest.java

--- abstraction/AkbilTest.java
+++ abstraction/AkbilTest.java
@@ -19,6 +19,7 @@ public class AkbilTest {
 			p.printInfo();
 		}
 		
+		/* bu kısımda yaş artışından sonra manuel olarak nesnelerin tip dönüşümü yapılıyor.
 		System.out.println("Yaş Artışı");
 		for(Person p:person){
 			p.addAge();
@@ -27,6 +28,27 @@ public class AkbilTest {
 		
 		person[1] = new Employee(person[1].getId(), person[1].getName(), person[1].getCredit(), person[1].getAge());
 		person[2] = new Retired(person[2].getId(), person[2].getName(), person[2].getCredit(), person[2].getAge());
+		*/
+		
+		System.out.println("Yaş Artışı ve Tip Değişimi");
+		for(int i=0; i<4 ; i++){
+			person[i].addAge();
+			if( (person[i] instanceof Student && person[i].getAge()<20) || 
+			    (person[i] instanceof Employee && person[i].getAge()<60) || 
+			    (person[i] instanceof Retired && person[i].getAge()>=60))
+				    person[i].printInfo();
+				
+			else if( person[i] instanceof Student && person[i].getAge()>=20 ){
+				 person[i] = new Employee(person[i].getId(),person[i].getName(),person[i].getCredit(),person[i].getAge());
+				    person[i].printInfo();
+				
+			}
+			else if( person[i] instanceof Employee && person[i].getAge()>=60 ){
+				person[i] = new Retired(person[i].getId(),person[i].getName(),person[i].getCredit(),person[i].getAge());
+				person[i].printInfo();
+				
+			}
+		}
 		
 		System.out.println("Otobüse Biniş-2");
 		for(Person p: person){
